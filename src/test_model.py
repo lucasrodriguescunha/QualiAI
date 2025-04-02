1d48a51ffc75ea4e433a49d9dc6a85d8a99047b6
Yan Pedro <yanpzr1@gmail.com>
2025-04-02 10:39:23 -0300

    fix: correção no caminho de importação do modelo

--- src/test_model.py
+++ src/test_model.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import os
 
 # Carregar o modelo treinado
-model = keras.models.load_model("apple_defect_model.h5")
+model = keras.models.load_model("QualiAI/apple_defect_model.h5")
 
 # Caminho para a imagem de teste
 img_path = "QualiAi/test3.png"  # Atualize com o caminho correto
